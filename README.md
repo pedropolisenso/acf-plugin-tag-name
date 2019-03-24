d4cc659f50571061a7b3f9270119388cd42700d3
pedro filho <pedro.filho@b2wdigital.com>
2019-03-24 17:23:20 +0000

    update README and remove space

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ACF TAG-NAME Field
 
-TAG-NAME is a custom field with a feature of tag name
+TAG-NAME is a custom field with a feature of tag name that help you as auxiliar using the [ACF Plugin](https://www.advancedcustomfields.com/)
 
 ### Compatibility
 
@@ -8,8 +8,10 @@ This ACF field type is compatible with:
 * ACF 5
 * ACF 4
 
-### Installation
+### Installing
 
 1. Download project in format `.zip`
 2. Copy the `acf-plugin-tag-name` folder into your `wp-content/plugins` folder
 3. Active the plugin
+
+> Have fun!
--- acf-TAG_NAME.php
+++ acf-TAG_NAME.php
@@ -1,92 +1,71 @@
 <?php
-
-/*
-Plugin Name: Custom Fields: TAG-NAME
-Plugin URI: https://github.com/pedropolisenso/acf-plugin-tag-name
-Description: Custom field for feature tag name
-Version: 1.0.0
-Author: Pedro Polisenso
-Author URI: https://github.com/pedropolisenso
-License: GPLv2 or later
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
-*/
-
-// exit if accessed directly
-if( ! defined( 'ABSPATH' ) ) exit;
-
-
-// check if class already exists
-if( !class_exists('acf_plugin_TAG_NAME') ) :
-
-class acf_plugin_TAG_NAME {
-	
 	/*
-	*  __construct
-	*
-	*  This function will setup the class functionality
-	*
-	*  @type	function
-	*  @date	17/02/2016
-	*  @since	1.0.0
-	*
-	*  @param	n/a
-	*  @return	n/a
+	Plugin Name: Custom Fields: TAG-NAME
+	Plugin URI: https://github.com/pedropolisenso/acf-plugin-tag-name
+	Description: Custom field for feature tag name
+	Version: 1.0.5
+	Author: Pedro Polisenso
+	Author URI: https://github.com/pedropolisenso
+	License: GPLv2 or later
+	License URI: http://www.gnu.org/licenses/gpl-2.0.html
 	*/
-	
-	function __construct() {
+
+	if( ! defined( 'ABSPATH' ) ) exit;
+	if( !class_exists('acf_plugin_TAG_NAME') ) :
+
+	class acf_plugin_TAG_NAME {	
+		/*
+		*  __construct
+		*
+		*  This function will setup the class functionality
+		*
+		*  @type	function
+		*  @since	1.0.5
+		*
+		*  @param	n/a
+		*  @return	n/a
+		*/
 		
-		// vars
-		$this->settings = array(
-			'version'	=> '1.0.0',
-			'url'		=> plugin_dir_url( __FILE__ ),
-			'path'		=> plugin_dir_path( __FILE__ )
-		);
+		function __construct() {
+			
+			// vars
+			$this->settings = array(
+				'version'	=> '1.0.5',
+				'url'		=> plugin_dir_url( __FILE__ ),
+				'path'		=> plugin_dir_path( __FILE__ )
+			);
+			
+			// set text domain
+			// https://codex.wordpress.org/Function_Reference/load_plugin_textdomain
+			load_plugin_textdomain( 'acf-TAG_NAME', false, plugin_basename( dirname( __FILE__ ) ) . '/lang' ); 
+			
+			// include field
+			add_action('acf/include_field_types', 	array($this, 'include_field_types')); // v5
+			add_action('acf/register_fields', 		array($this, 'include_field_types')); // v4
+		}
+
+		/*
+		*  include_field_types
+		*
+		*  This function will include the field type class
+		*
+		*  @type	function
+		*  @since	1.0.5
+		*
+		*  @param	$version (int) major ACF version. Defaults to false
+		*  @return	n/a
+		*/
 		
-		
-		// set text domain
-		// https://codex.wordpress.org/Function_Reference/load_plugin_textdomain
-		load_plugin_textdomain( 'acf-TAG_NAME', false, plugin_basename( dirname( __FILE__ ) ) . '/lang' ); 
-		
-		
-		// include field
-		add_action('acf/include_field_types', 	array($this, 'include_field_types')); // v5
-		add_action('acf/register_fields', 		array($this, 'include_field_types')); // v4
-		
+		function include_field_types( $version = false ) {		
+			// support empty $version
+			if( !$version ) $version = 4;
+
+			// include
+			include_once('fields/acf-TAG_NAME-v' . $version . '.php');
+		}
 	}
-	
-	
-	/*
-	*  include_field_types
-	*
-	*  This function will include the field type class
-	*
-	*  @type	function
-	*  @date	17/02/2016
-	*  @since	1.0.0
-	*
-	*  @param	$version (int) major ACF version. Defaults to false
-	*  @return	n/a
-	*/
-	
-	function include_field_types( $version = false ) {
-		
-		// support empty $version
-		if( !$version ) $version = 4;
-		
-		
-		// include
-		include_once('fields/acf-TAG_NAME-v' . $version . '.php');
-		
-	}
-	
-}
 
+	new acf_plugin_TAG_NAME();
 
-// initialize
-new acf_plugin_TAG_NAME();
-
-
-// class_exists check
-endif;
-	
+	endif;	
 ?>
